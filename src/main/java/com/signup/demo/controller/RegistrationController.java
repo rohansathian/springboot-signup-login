e3e103e85a951733c8b67160718eb822ce14835b
Rohan vs <rohanssar523@gmail.com>
2025-11-22 12:54:20 +0530

    Initial commit

---
+++ src/main/java/com/signup/demo/controller/RegistrationController.java
@@ -0,0 +1,28 @@
+package com.signup.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.signup.demo.model.MyAppUser;
+import com.signup.demo.model.MyAppUserRepository;
+
+@RestController
+public class RegistrationController {
+
+    @Autowired
+    private MyAppUserRepository myAppUserRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    
+    @PostMapping(value = "/req/signup", consumes ="application/json")
+    public MyAppUser createUser(@RequestBody MyAppUser user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return myAppUserRepository.save(user);
+    }
+    
+}
---
+++ src/main/java/com/signup/demo/security/SecurityConfig.java
@@ -0,0 +1,68 @@
+package com.signup.demo.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.signup.demo.model.MyAppUserService;
+
+import lombok.AllArgsConstructor;
+
+
+@Configuration
+@AllArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig { 
+    // Security configuration details would go here
+
+    @Autowired
+    private final MyAppUserService appUserService;
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return appUserService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(appUserService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity
+                 .csrf(AbstractHttpConfigurer::disable)
+                 .formLogin(httpForm ->{
+                     httpForm.loginPage("/login").permitAll();
+                     httpForm.defaultSuccessUrl("/index", true);
+                          
+                 })
+                 
+                 .authorizeHttpRequests(registry ->{
+                     registry.requestMatchers("/dev/**", "/req/signup", "/css/**", "/js/**", "/login").permitAll();
+                     registry.anyRequest().authenticated();
+                    }) 
+
+                 .build();
+    }
+    
+}
